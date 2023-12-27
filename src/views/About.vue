b413b473dfe9b96e5010a0eadea26f656391ba36
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-12-27 14:56:57 +1100

    0.6.2

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
         and based off
         <a href="https://electrics01.com/mapit">Mapit</a>
       </p>
-      <p class="small">Version: 0.6.1</p>
+      <p class="small">Version: 0.6.2</p>
     </div>
   </div>
 </template>
