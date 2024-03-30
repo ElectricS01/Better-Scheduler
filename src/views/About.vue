a0352b83f7cc8c6b6a26bf78eddfdee768fb1706
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-03-30 16:45:46 +1100

    0.10.0

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="container-flex">
     <div class="menu">
       <p class="title-menu">About BetterScheduler</p>
-      <img src="/192.png" height="144" width="144" alt="Logo" />
+      <img src="../assets/logo.png" height="144" width="144" alt="Logo" />
       <p class="small">
         BetterScheduler is an interactive and simple tool to build schedules for
         school, work, and home
@@ -13,7 +13,7 @@
         and based off
         <a href="https://electrics01.com/mapit">Mapit</a>
       </p>
-      <p class="small">Version: 0.9.2</p>
+      <p class="small">Version: 0.10.0</p>
     </div>
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,22 @@ import vue from "@vitejs/plugin-vue"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "Better Scheduler",
+        short_name: "Scheduler",
+        description:
+          "BetterScheduler is an interactive and simple tool to build schedules for school, work, and home",
+        theme_color: "#282a2b",
+        background_color: "#181a1b",
+        display: "fullscreen",
+        categories: ["productivity", "events", "lifestyle", "utilities"],
+        start_url: "/scheduler"
+      }
+    })
+  ],
   server: {
     port: 8080
   }
