63717d465b5d57439ad89a8ebc8a9655a07afd39
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-07-08 20:34:00 +1000

    0.10.11

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <p class="title">Error 404: page not found</p>
-    <router-link to="scheduler" class="menu-button">
+    <router-link to="/scheduler" class="menu-button">
       Go to the Scheduler
     </router-link>
   </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
         and based off
         <a href="https://electrics01.com/mapit">Mapit</a>
       </p>
-      <p class="small">Version: 0.10.10</p>
+      <p class="small">Version: 0.10.11</p>
     </div>
   </div>
 </template>
